f415dc7e60d57729dacf1a3ca6a671ef2224a69f
C-BdB <cyp.42@hotmail.fr>
2023-12-12 23:06:16 +0100

    Delete unrelevant comment

--- function/handler.py
+++ function/handler.py
@@ -9,7 +9,6 @@ def lambda_handler(event, context):
     print("CloudWatch log group name:",  context.log_group_name)
     print("Lambda Request ID:", context.aws_request_id)
     print("Lambda function memory limits in MB:", context.memory_limit_in_mb)
-    # We have added a 1 second delay so you can see the time remaining in get_remaining_time_in_millis.
     return {
         "statusCode": 200,
         "headers": {
